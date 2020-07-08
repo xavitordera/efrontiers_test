5105396f1b4197b6bc2b0b20c20ddbb53fa85cf7
Xavi Tordera <xavier.tordera@slashmobility.com>
2020-07-08 16:39:25 +0200

    Project creation

---
+++ EfrontiersTest/EfrontiersTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EfrontiersTest
+//
+//  Created by Xavi Tordera on 08/07/2020.
+//  Copyright © 2020 Xavi Tordera. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
